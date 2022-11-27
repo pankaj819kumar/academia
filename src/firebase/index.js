906e48c668b591a4dbf8a1ffc59a7fb84584ad49
Pankaj Kumar <pankaj819kumar@gmail.com>
2022-11-27 21:47:42 +0530

    environment variable set up in github

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -15,7 +15,6 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
